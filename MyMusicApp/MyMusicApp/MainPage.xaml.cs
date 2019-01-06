acb7dce765fcff99ea8888cb9ed588b537817fc6
wakabayashi <keitarowakabayashinit@gmail.com>
2019-01-07 00:47:38 +0900

    first init

--- MyMusicApp/MyMusicApp/MainPage.xaml.cs
+++ MyMusicApp/MyMusicApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyMusicApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,16 @@ namespace MyMusicApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void OnXamarinButtonClicked(object sender, EventArgs args)
+        {
+            DependencyService.Get<IWebBrowserService>().Open(new Uri("https://docs.microsoft.com/ja-jp/xamarin/"));
+        }
+
+        void OnMusicButtonClicked(object sender, EventArgs args)
+        {
+            DependencyService.Get<IMusicService>().PlayMusic(null);
         }
     }
 }
